a7473b4fa315d38c895ddf9dea6ee951c62d1a10
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2019-03-20 14:40:03 -0500

    Cleanup head and add test

--- tubekit.rb
+++ tubekit.rb
@@ -1,7 +1,6 @@
 class Tubekit < Formula
   desc "Tool that helps you to operate Kubernetes clusters more effectively"
   homepage "https://github.com/reconquest/tubekit"
-  head "https://github.com/reconquest/tubekit.git"
   url "https://github.com/reconquest/tubekit/releases/download/v3/tubekit_3_Darwin_x86_64.tar.gz"
   sha256 "b966e7b014e0d16e22f0d15dbd7c80a084160c6851bf7ad767d0085ac3ac10d1"
 
@@ -11,5 +10,10 @@ class Tubekit < Formula
 
   def install
     bin.install "tubectl"
+  end
+
+  test do
+    system "#{bin}/tubectl", "--tube-version"
+    assert_match /usage/i, shell_output("#{bin}/tubectl --tube-help")
   end
 end
