467d293474ca9e4cafe9d658e2fd9ae98ac35e76
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2022-06-09 21:04:20 -0500

    Bring back `depends_on`

--- tubekit.rb
+++ tubekit.rb
@@ -4,6 +4,8 @@ class Tubekit < Formula
   url "https://github.com/reconquest/tubekit/releases/download/v3/tubekit_3_Darwin_x86_64.tar.gz"
   sha256 "b966e7b014e0d16e22f0d15dbd7c80a084160c6851bf7ad767d0085ac3ac10d1"
 
+  depends_on "kubernetes-cli"
+
   def install
     bin.install "tubectl"
   end
